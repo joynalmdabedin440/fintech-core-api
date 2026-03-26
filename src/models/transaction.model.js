1172a9fd46a208982022aab4401b538a6bb21a3f
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-26 23:13:09 +0600

    add transaction to account schema

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -6,5 +6,11 @@ const transactionSchema = new mongoose.Schema({
         ref: "account",
         required: [true, "transaction need a from account"],
         index:true
+    },
+    toAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "transaction need a to account"],
+        index:true
     }
 })
