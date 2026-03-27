552da17a30e18bd592d0b65efc2e62d98a58fa50
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-27 22:12:31 +0600

    add ledger schema

---
+++ src/models/ledger.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const ledgerSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        required: [true, "Account need for ledger"],
+        immutable: true,
+        index:true
+        
+        
+    },
+    amount: {
+        type: Number,
+        required: [true, "ledger need to provide amount"],
+        immutable:true
+    },
+
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "transaction",
+        required:[true,"ledger associated with transaction"],
+        immutable: true,
+        index:true
+    },
+    types: {
+        type: String,
+        enum: {
+            values: ["DEBIT", "CREDIT"],
+            message:"Types can be either DEBIT or CREDIT"
+        },
+        required: [true, "Need to provide types"],
+        immutable:true
+        
+    }
+
+})
+
