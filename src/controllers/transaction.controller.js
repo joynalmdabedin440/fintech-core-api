8dbad66728e1f58ab0ac296d0674f0914b140d16
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-29 21:34:55 +0600

    add change

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,7 +4,7 @@ async function transactionController(req,res) {
     const { fromAccount, toAccount, status, amount, idempotencyKey } = req.body
     
     try {
-        const transaction = await transactionModel.createTransaction(fromAccount, toAccount, status, amount, idempotencyKey)
+        const transaction = await transactionModel.createTransaction(fromAccount, toAccount, amount, idempotencyKey)
         res.status(201).json(transaction)
     } catch (error) {
         res.status(500).json({ error: error.message })
