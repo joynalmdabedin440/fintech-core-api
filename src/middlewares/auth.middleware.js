f2151496baa3f0ebb22abe4c4ac815a269071ad7
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-04-11 23:38:15 +0600

    add system user middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -28,4 +28,33 @@ async function authMiddleWare(req, res, next) {
     
 }
 
+async function authSystemUserMiddleWare(req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+    
+    if (!token) {
+        return res.status(401).json({
+            msg:"Unauthorized user,token is missing"
+        })
+    }
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        const user = await userModel.findById(decoded.id).select("+systemAccount")
+        if (!user) {
+            return res.status(401).json({
+                msg:"Invalid user token"
+            })
+        }
+        req.user = user
+        return next()
+    } catch (error) {
+        return res.status(401).json({
+            msg:"Invalid user token"
+        })
+    }
+
+ }
+
 module.exports={authMiddleWare}
